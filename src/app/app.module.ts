c0eda286b65f0afd00793a0a3442d1bab87b903c
linhnv296 <linhnv296@gmail.com>
2019-07-29 09:15:11 +0700

    Colorpick Pet Calculator Background

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PetComponent } from './pet/pet.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetComponent,
+    CalculatorComponent,
+    ColorPickerComponent
   ],
   imports: [
     BrowserModule,
